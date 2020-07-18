fe8772d558310049b057ac988a246b4f4d2ddfac
Eduardo Gómez <eduardocardenas97@gmail.com>
2020-07-18 12:02:49 -0400

    Refeactor 2 - URLs in JSON

--- index.js
+++ index.js
@@ -1,37 +1,24 @@
-'use strict'
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+
+let url = {
+    'docker': 'mongodb://127.0.0.1:2700/?useUnifiedTopology=true',
+    'local': 'mongodb://localhost:27000/?useUnifiedTopology=true',
+    'atlas': 'mongodb+srv://apiDevelop:0982@cluster0.yrdbq.gcp.mongodb.net/reservation?retryWrites=true&w=majority&useUnifiedTopology=true',
+};
+
 const dbName = 'reservation';
-const mongoRun = 'docker'
+let app = require('./app');
+let port = 3000;
 
-
-var app = require('./app');
-var port = 3000;
-
-
-switch(mongoRun){
-    case 'docker':
-        const urlDocker = 'mongodb://127.0.0.1:2700/?useUnifiedTopology=true';
-        var client = new MongoClient(urlDocker);
-        break;
-    case 'local':
-        const urlLocal = 'mongodb://localhost:27000/?useUnifiedTopology=true';
-        var client = new MongoClient(urlLocal);
-        break;
-    default:
-        const urlAtlas = 'mongodb+srv://apiDevelop:0982@cluster0.yrdbq.gcp.mongodb.net/reservation?retryWrites=true&w=majority&useUnifiedTopology=true';
-        var client = new MongoClient(urlAtlas);
-        break;
-}
+let client = new MongoClient(url.docker);
 // Use connect method to connect to the Server
 client.connect(function(err) {
     assert.equal(null, err);
-    console.log(`Connected successfully to MongoDB ${mongoRun}`);
-    const db = client.db(dbName);
+    console.log(`Connected successfully to MongoDB `);
+    exports.MongoClient = client.db(dbName);
     app.listen(port, () => {
-        console.log("NodeJS server running on http://localhost:3000");
+        console.log(`NodeJS server running on http://localhost:${port}`);
     });
     client.close();
 });
-
-
