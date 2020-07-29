18002edb6db658d45ebdffc8fe878e77245e6f1b
Eduardo Gómez <eduardocardenas97@gmail.com>
2020-07-28 20:24:38 -0400

    Rename: /routes/ .routes.js

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ let express = require('express');
 let bodyParser = require('body-parser');
 let app = express();
 
-let reservation = require('./src/routes/reservationRoutes'); 
-let user = require('./src/routes/userRoutes');
+let reservation = require('./src/routes/reservation.routes'); 
+let user = require('./src/routes/user.routes');
+let product = require('./src/routes/product.routes');
 
 //Configuramos bodyParser para que convierta el body de nuestras peticiones a JSON
 
